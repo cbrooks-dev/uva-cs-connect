78a8c96a8c566b02fc217494ece3bd02ac1a7d26
maddiew1se <faa2fw@virginia.edu>
2025-11-24 18:57:28 -0500

    Register

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from .db import execute
 
 bp = Blueprint('main', __name__)
@@ -16,6 +16,40 @@ def index():
 @bp.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        grad_year = request.form.get('grad_year')
+        password = request.form['password']
+
+        existing = execute(
+            "SELECT * FROM Student WHERE email = %s",
+            (email,),
+            fetchone=True
+        )
+
+        if existing:
+            flash("An account with that email already exists.")
+            return redirect(url_for('main.register'))
+
+        execute(
+            """
+            INSERT INTO Student (first_name, last_name, email, grad_year, password)
+            VALUES (%s, %s, %s, %s, %s)
+            """,
+            (first_name, last_name, email, grad_year, generate_password_hash(password)),
+            commit=True
+        )
+
+        flash("Registration successful! You can now log in.")
+        return redirect(url_for('main.login'))
+
+    return render_template('register.html')
+
 
 @bp.route('/about', methods=['GET'])
 def about():
